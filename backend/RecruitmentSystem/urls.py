2c0370d337d99ccd10544c7b2b055ee406c18c74
Developer-felix <onjombafelix1@gmail.com>
2021-03-07 04:24:56 -0800

    customized admin pannel

--- backend/RecruitmentSystem/urls.py
+++ backend/RecruitmentSystem/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+admi.site.site_header = "Job Recruitment System"
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
