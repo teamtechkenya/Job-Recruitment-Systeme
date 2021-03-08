0fb0d3ae537e7f64ac67a1667a5c9d6d58746dca
Developer-felix <onjombafelix1@gmail.com>
2021-03-08 08:34:43 -0800

    configured all the user app settings

--- backend/RecruitmentSystem/settings.py
+++ backend/RecruitmentSystem/settings.py
@@ -120,3 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+
+
+LOGIN_REDIRECT_URL = 'dashbord'
+LOGIN_URL = 'login'
