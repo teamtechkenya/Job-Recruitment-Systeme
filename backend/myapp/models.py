4b982a340c21ed139b2dd91d9f66a6856dfbe20f
Gateway47 <itsregalo047@gmail.com>
2021-03-11 12:30:36 +0300

    added Application and Employer model

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -17,6 +17,7 @@ TYPE_CHOICES = [
 class Jobs(models.Model):
     name = models.CharField(max_length=255)
     jobtype = models.CharField(max_length=15,choices=TYPE_CHOICES)
+    employer = models.ForeignKey("Employer", on_delete=models.CASCADE)
     address = models.CharField(max_length=255)
     datecreated = models.DateTimeField(editable=False)
     salary = models.IntegerField()
@@ -31,6 +32,10 @@ class Jobs(models.Model):
         ''' On save, update timestamps '''
         if not self.id:
             self.datecreated = timezone.now()
+            
+    @property
+    def no_of_applications(self):
+        return Jobs.objects.filter(Jobs=self).count()
 
 class Applicant(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
@@ -42,3 +47,24 @@ class Applicant(models.Model):
     def __str__(self):
         return self.name
 
+class Employer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    address = models.CharField(max_length=255)
+    
+    @property
+    def employer_jobs(self):
+        return Jobs.objects.filter(employer=self)
+    
+    def __str__(self):
+        return self.name
+    
+class Application(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    job = models.ForeignKey(Jobs, on_delete=models.CASCADE)
+    quote = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.user.username
